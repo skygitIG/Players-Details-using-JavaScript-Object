b9e17693674896d4f35961a53d5d3a8593f70f26
Akash Kumar <117715724+skygitIG@users.noreply.github.com>
2023-05-14 14:54:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,65 @@
+//Shubman Gill
+
+let shubhman = {
+    Name: "Name : Shubman Gill",
+    High_score: "High Score" + " " + ":" + " " + 208,
+    Type: "Category : Batter",
+    Game: "Game : Cricket"
+};
+
+function shubhman1() {
+    document.getElementById("info").innerHTML = shubhman.Name + "<br>" + shubhman.High_score + "<br>" + shubhman.Type + "<br>" + shubhman.Game;
+}
+
+//Smriti Mandhana
+
+let smriti = {
+    Name: "Name : Smriti Mandhana",
+    High_score: "High Score" + " " + ":" + " " + 135,
+    Type: "Category : Batter",
+    Game: "Game : Cricket"
+};
+
+function smriti1() {
+    document.getElementById("info").innerHTML = smriti.Name + "<br>" + smriti.High_score + "<br>" + smriti.Type + "<br>" + smriti.Game;
+}
+
+//Virat Kohli
+
+let virat = {
+    Name: "Name : Virat Kohli",
+    High_score: "High Score" + " " + ":" + " " + 254,
+    Type: "Category : Batter",
+    Game: "Game : Cricket"
+};
+
+function virat1() {
+    document.getElementById("info").innerHTML = virat.Name + "<br>" + virat.High_score + "<br>" + virat.Type + "<br>" + virat.Game;
+}
+
+//Axar Patel
+
+let axar = {
+    Name: "Name : Axar Patel",
+    High_score: "High Score" + " " + ":" + " " + 84,
+    High_wicket: "Highest Wicket" + " " + ":" + " " + 6,
+    Type: "Category : All-Rounder",
+    Game: "Game : Cricket"
+};
+
+function axar1() {
+    document.getElementById("info").innerHTML = axar.Name + "<br>" + axar.High_score + "<br>" + axar.High_wicket + "<br>" + axar.Type + "<br>" + axar.Game;
+}
+
+//Sunil Chhetri
+
+let sunil = {
+    Name: "Name : Sunil Chhetri",
+    High_score: "Total Goals" + " " + ":" + " " + 85,
+    Type: "Category : Link-up play",
+    Game: "Game : Football"
+};
+
+function sunil1() {
+    document.getElementById("info").innerHTML = sunil.Name + "<br>" + sunil.High_score + "<br>" + sunil.Type + "<br>" + sunil.Game;
+} 
